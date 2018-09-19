267efa540932255ded5f56bae6e7a19a8a81e22b
lishaokun <717735344@qq.com>
2018-09-19 16:44:53 +0800

    数据标准化输出ok

--- client/client.php
+++ client/client.php
@@ -1,4 +1,18 @@
 <?php  
 	//客户端发起api请求
+
+	include './vendor/autoload.php';
+
+	$url = 'http://localhost/apis/server/server.php';
+
+	$curl = new Curl\Curl();
+	$curl->get($url);
+
+	if ($curl->error) {
+	    echo $curl->error_code;
+	}
+	else {
+	    echo $curl->response;
+	}
 	
 ?>
--- server/server.php
+++ server/server.php
@@ -1,4 +1,24 @@
 <?php  
-	//服务器端接收api请求
+	//服务器端接收api请求 响应  json 
 	
+	$pdo = new PDO('mysql:host=localhost;dbname=api;charset=utf8','root','');
+
+	$stmt = $pdo->query('select * from user');
+
+	$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+	//定义标准化产出数据格式函数
+	function resp($data,$status,$message) {
+
+		$res = [
+			'status'=>$status, //服务器响应状态码
+			'message'=>$message,
+			'data'=>$data //此次api请求的描述
+
+		];
+
+		return json_encode($res,JSON_UNESCAPED_UNICODE);
+	}
+
+	echo resp($data,200,'success');
 ?>
